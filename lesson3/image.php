301860386fcd79944fe32c87438bf550c493d4f8
KateVashurina <wasturina.ekatterina@gmail.com>
2022-01-01 18:11:18 +0300

    lesson4

--- lesson3/image.php
+++ lesson3/image.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'Twig/Autoloader.php';
+// include 'config.php';
+
 Twig_Autoloader::register();
 
 try {
--- lesson3/index.php
+++ lesson3/index.php
@@ -1,33 +1,49 @@
 <?php
-const PATH_TO_BIG = 'src/images/big/';
-const PATH_TO_SMALL = 'src/images/small/';
+define('DB_DRIVER','mysql');
+define('DB_HOST','localhost');
+define('DB_NAME','gallery');
+define('DB_USER','root');
+define('DB_PASS','root');
 
 require_once 'Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-try {
-    $dbh = new PDO('mysql:dbname=gallery;host=localhost', 'root', 'root');
-} catch (PDOException $e) {
-    echo "Error: Could not connect. " . $e->getMessage();
-}
-
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-try {
-    $sql = "SELECT * FROM images";
-    $sth = $dbh->query($sql);
-    while ($row = $sth->fetchObject()) {
-        $data[] = $row;
+try
+{ 
+	$connect_str = DB_DRIVER . ':host='. DB_HOST . ';dbname=' . DB_NAME;
+	$db = new PDO($connect_str,DB_USER,DB_PASS);
+  
+	$error_array = $db->errorInfo();
+ 
+	if($db->errorCode() != 0000)
+	    echo "SQL ошибка: " . $error_array[2] . '<br />';
+  
+    if(isset($_GET['limit'])) {
+        $limit = $_GET['limit'];
+    } else {
+        $limit = 3;
     }
-    unset($dbh);
+	$result = $db->query("SELECT * FROM `images` LIMIT $limit");
+     
+	while($row = $result->fetch())
+	{
+        $data[] = $row;
+	}
+    // unset($db);
 
     $loader = new Twig_Loader_Filesystem('src/templates');
     $twig = new Twig_Environment($loader);
     $template = $twig->loadTemplate('gallery.tmpl');
     echo $template->render(array (
-        'data' => $data
+        'data' => $data,
+        'count' => count($data)
     ));
-} catch (Exception $e) {
-    die ('Error: ' . $e->getMessage());
 }
+catch(PDOException $e)
+{
+	die("Error: ".$e->getMessage());
+}
+
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
